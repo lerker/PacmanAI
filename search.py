52c6cea5bbdd0c32da1ec7b77ff4cf3768774da7
Nelson E Ponzoni Cuadra <nelson.ponzoni@ar.ey.com>
2019-03-04 15:42:51 -0300

    base

--- search.py
+++ search.py
@@ -11,12 +11,6 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-"""
-https://github.com/LuLi0077/AI/tree/master/Pacman/search
-
-https://github.com/dmurtari/Pacman/blob/master/search.py
-
-"""
 
 """
 In search.py, you will implement generic search algorithms which are called by
@@ -93,44 +87,16 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
-    '''This function pushes non-visited nodes onto the stack.
-    Nodes are popped one by one, and the following steps are performed:
-    1. The node is marked as visited.
-    2. If it is a goal node, the loop stops, and the solution is obtained by backtracking using stored parents.
-    3. If it is not a goal node, it is expanded.
-    4. If the successor node is not visited, then it is pushed onto the stack and its parent is stored.'''
-    #print("Start:", problem.getStartState())
-    #print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
-    #print("Start's successors:", problem.getSuccessors(problem.getStartState()))
-
-    # Use the genericSearch method, with the fringe maintained using a Stack
-    # so that the search proceeds in the order of exploring from the node last
-    # discovered
-    return genericSearch(problem, util.Stack())
-
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-
-    # Use the genericSearch method, with the fringe maintained with a Queue so 
-    # that all nodes on the same level are explored before the next level is 
-    # explored. This will find the optimal solution, because each level is 
-    # explored before the next, so the first time the goal is reached will be
-    # the shortest path to the goal.
-    return genericSearch(problem, util.Queue())
-
     util.raiseNotDefined()
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    
-    # Running UCS is the same as running A* Search with a null heuristic, 
-    # so simplify the calls by just using aStarSearch.
-    return aStarSearch(problem)
-
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
@@ -143,84 +109,9 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-
-    # Use the genericSearch method, with the fringe maintained with a 
-    # PriorityQueue. The cost is calculated using the provided heuristic. 
-    # If no heuristic is given (such as UCS), then default to the given
-    # nullHeuristic
-    return genericSearch(problem, util.PriorityQueue(), heuristic)
-
     util.raiseNotDefined()
 
 
-
-
-def genericSearch(problem, fringe, heuristic=None):
-    """
-    https://github.com/dmurtari/Pacman/blob/master/search.py
-    Generic search algorithm that takes a problem and a queuing strategy
-    and performs a search given that strategy
-    Written Answers for Question 1
-    1. The exploration order is what I would have expected. The search goes as
-       deep as it can, before exploring other paths (as would be expected with 
-       depth first search).
-    2. No, Pacman does not go to all of the explored squares on the way to the 
-       goal. He follows a path that does not lead him to any dead-ends, even if
-       dead ends were explored. In my implementation, the length for a 
-       MediumMaze was 130.
-    3. This is not the least-cost solution -- there is clearly a cheaper 
-       solution that Pacman does not take on the MediumMaze. This is because
-       DFS will return the first solution that it finds that solves the problem,
-       not the best solution.
-    Written Answers for Question 4
-    1. With OpenMaze, BFS, UCS, and A* all find the shortest path to the goal, 
-       with BFS and UCS expanding the same number of search nodes (682) and A*
-       expanding the least amount of nodes (535). All paths have a cost of 54.
-       DFS does find the solution as well, but is not the shortest -- the cost
-       of the path found with DFS was 298. This is because DFS does not look 
-       for the shortest path.
-    """
-
-    visited = []        # List of already visisted nodes
-    action_list = []    # List of actions taken to get to the current node
-    initial = problem.getStartState()   # Starting state of the problem
-
-    # Push a tuple of the start state and blank action list onto the given
-    # fringe data structure. If a priority queue is in use, then calculate
-    # the priority using the heuristic
-    if isinstance(fringe, util.Stack) or isinstance(fringe, util.Queue):
-        fringe.push((initial, action_list))
-    elif isinstance(fringe, util.PriorityQueue):
-        fringe.push((initial, action_list), heuristic(initial, problem))
-
-    # While there are still elements on the fringe, expand the value of each 
-    # node for the node to explore, actions to get there, and the cost. If the
-    # node isn't visited already, check to see if node is the goal. If no, then
-    # add all of the node's successors onto the fringe (with relevant 
-    # information about path and cost associated with that node)
-    while fringe: 
-        if isinstance(fringe, util.Stack) or isinstance(fringe, util.Queue):
-            node, actions = fringe.pop() 
-        elif isinstance(fringe, util.PriorityQueue):
-            node, actions = fringe.pop()
-        
-        if not node in visited:
-            visited.append(node)
-            if problem.isGoalState(node):
-                return actions
-            successors = problem.getSuccessors(node)
-            for successor in successors:
-                coordinate, direction, cost = successor
-                newActions = actions + [direction]
-                if isinstance(fringe, util.Stack) or isinstance(fringe, util.Queue):
-                    fringe.push((coordinate, newActions))
-                elif isinstance(fringe, util.PriorityQueue):
-                    newCost = problem.getCostOfActions(newActions) + \
-                               heuristic(coordinate, problem)
-                    fringe.push((coordinate, newActions), newCost)                  
-
-    return []
-    
 # Abbreviations
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
